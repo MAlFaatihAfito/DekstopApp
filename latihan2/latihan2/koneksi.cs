139955e80060e87873a90b613fd85e9e36a6d363
FathXryn <143806722+MAlFaatihAfito@users.noreply.github.com>
2023-09-01 19:17:46 +0700

    Add files via upload

---
+++ latihan2/latihan2/koneksi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan2
+{
+    internal class koneksi
+    {
+        static string conn;
+
+        public static SqlConnection Conn
+        {
+            get
+            {
+                return new SqlConnection(conn);
+            }
+        }
+
+        static koneksi()
+        {
+            string connStr = "server=localhost;" +
+                "database=latihan1;" +
+                "Integrated Security=True";
+            conn = connStr;
+        }
+    }
+}
